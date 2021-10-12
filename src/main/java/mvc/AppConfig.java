6d7ecccb6c79efef11d1b65872571b6e0234ba74
DawidRozewski <dawid.rrozewski@gmail.com>
2021-10-12 21:14:02 +0200

    Podstawowe klasy MVC.

---
+++ src/main/java/mvc/AppConfig.java
@@ -0,0 +1,13 @@
+package mvc;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "mvc")
+public class AppConfig implements WebMvcConfigurer {
+}
